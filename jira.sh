05c2815d6a7928ea0542348d99babe1add8183a0
Damon Baker <damonb@familyzone.com>
2020-07-27 19:15:45 +0800

    Tidy up + Fix issue with opening ticket directly

--- jira.sh
+++ jira.sh
@@ -2,8 +2,7 @@
 
 . ~/fuzzy-jira/.jiraconfig
 
-function jira() {
-    [[ ! -d ${JIRA_CACHE_DIR} ]] && mkdir ${JIRA_CACHE_DIR}
+jira() {
     if [[ $1 == "fetch" ]]; then
         if [[ -n "$2" ]]; then
             PROJECT=$(tr a-z A-Z <<< "$2")
@@ -14,6 +13,8 @@ function jira() {
         jira_fetch "$PROJECT"
     elif [[ $1 == "." ]]; then
         parse_ticket_key
+    elif [[ -n $(echo "$1" | grep -e '[A-Z]\+-[0-9]\+' -o) ]]; then
+        open_issue "$1"
     else
         if [[ -n "$1" ]]; then
             PROJECT=$(tr a-z A-Z <<< "$1")
@@ -26,33 +27,39 @@ function jira() {
     fi
 }
 
-function jira_fetch() {
+jira_fetch() {
     local url="${JIRA_DOMAIN}/rest/api/2/search?jql=project=${PROJECT}&fields=summary&maxResults=1000"
-    local auth="${JIRA_USERNAME}:${JIRA_PASSWORD}"
     echo "Fetching issues for $PROJECT..."
     echo "From ${url}"
     [[ ! -f "$PROJECT" ]] && touch "${CACHE}"
-    local result=$(curl -s -u "${auth}" -X GET -H 'Content-type: application/json' "${url}" |
-        jq -r '.issues[] | .key + " " + .fields.summary' |
-        join -v 1 <(sort --version-sort -) <(sort --version-sort "${CACHE}") |
-        tee -a "${CACHE}" |
-        wc -l |
-        tr -d ' ')
+    local curl_args=(
+        --silent
+        --fail
+        --user "${JIRA_USERNAME}:${JIRA_PASSWORD}" 
+        --header 'Content-type: application/json'
+        --request GET "${url}"
+    )
+    local result=$(curl "${curl_args[@]}" \
+        | jq -r '.issues[] | .key + " " + .fields.summary' \
+        | join -v 1 <(sort --version-sort -) <(sort --version-sort "${CACHE}") \
+        | tee -a "${CACHE}" \
+        | wc -l \
+        | tr -d ' ')
     local total=$(< "${CACHE}" wc -l | tr -d ' ')
     echo "Success: $result issues added to cache (${total} total)"
 }
 
-function search_issues() {
+search_issues() {
     local issue=$(< "${CACHE}" fzf --tac | cut -d ' ' -f 1 | cat)
     [[ -n "$issue" ]] && open_issue "$issue"
 }
 
-function open_issue() {
+open_issue() {
     echo "Opening issue ${1}"
     open "${JIRA_DOMAIN}/browse/${1}"
 }
 
-function parse_ticket_key() {
+parse_ticket_key() {
     local key=$(git describe --contains --all | grep -e '[A-Z]\+-[0-9]\+' -o)
     if [[ -n "$key" ]]; then
         open_issue "$key"
