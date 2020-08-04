ba99bc78de27fa4379d4974d0f8e552fb96c9923
Damon Baker <damonb@familyzone.com>
2020-08-05 00:30:33 +0800

    Improve caching and error handling

--- jira.sh
+++ jira.sh
@@ -1,38 +1,53 @@
 #!/bin/bash
-set -e
 
-. ~/fuzzy-jira/.jiraconfig
+# Setup
+# -----------------------------------------------------------------------
+script_absolute_path() {
+    # https://stackoverflow.com/a/246128
+    local src="${BASH_SOURCE[0]}"
+    while [ -h "$src" ]; do
+        dir="$(cd -P "$(dirname "$src")" >/dev/null 2>&1 && pwd)"
+        src="$(readlink "$src")"
+        [[ $src != /* ]] && src="$dir/$src"
+    done
+    SCRIPT_DIR="$(cd -P "$(dirname "$src")" >/dev/null 2>&1 && pwd)"
+}
+
+script_absolute_path
+
+. "$SCRIPT_DIR"/.jiraconfig
+# -----------------------------------------------------------------------
 
 jira() {
     if [[ -z "$1" ]]; then
         usage
-        exit 0
+        return 0
     fi
     if [[ $1 == 'fetch' ]]; then
         if [[ -n "$2" ]]; then
-            set_project $2
+            set_project "$2"
         else
             echo 'Please specify a project to fetch from'
-            exit 1
+            return 1
         fi
         fetch_issues "$PROJECT"
     elif [[ $1 == '.' ]]; then
         parse_git_branch
     elif [[ -n $(echo "$1" | grep -e '[A-Za-z]\+-[0-9]\+' -o) ]]; then
-        open_issue $(tr a-z A-Z <<< "$1")
+        open_issue "$(tr a-z A-Z <<< "$1")"
     else
-        set_project $1
+        set_project "$1"
         search_issues "$PROJECT"
     fi
 }
 
 set_project() {
     PROJECT=$(tr a-z A-Z <<< "$1")
-    CACHE="${JIRA_CACHE_DIR}/${PROJECT}"
+    CACHE="${SCRIPT_DIR}/.cache/${PROJECT}"
 }
 
 fetch_issues() {
-    local url="${JIRA_DOMAIN}/rest/api/2/search?jql=project=${PROJECT}&fields=summary&maxResults=1000"
+    local url="${JIRA_URL}/rest/api/2/search?jql=project=${PROJECT}&fields=summary&maxResults=1000"
     echo "Fetching issues for $PROJECT..."
     echo "From ${url}"
     [[ ! -f "$PROJECT" ]] && touch "${CACHE}"
@@ -44,7 +59,13 @@ fetch_issues() {
         --header 'Content-type: application/json'
         --request GET "${url}"
     )
-    local result=$(curl "${curl_args[@]}" \
+    local response=$(curl "${curl_args[@]}")
+    if [[ -z "$response" ]]; then
+        # Remove empty cache on error
+        [[ -z $(cat "${CACHE}") ]] && rm "${CACHE}"
+        return 1
+    fi
+    local result=$(echo "${response}" \
         | jq -r '.issues[] | .key + " " + .fields.summary' \
         | join -v 1 <(sort --version-sort -) <(sort --version-sort "${CACHE}") \
         | tee -a "${CACHE}" \
@@ -66,9 +87,9 @@ open_issue() {
         (*Linux*) open_cmd='xdg-open' ;;
         (*Darwin*) open_cmd='open' ;;
         (*CYGWIN*) open_cmd='cygstart' ;;
-        (*) echo 'Error: Unsupported platform'; exit 1
+        (*) echo 'Error: Unsupported platform'; return 1
     esac
-    ${open_cmd} "${JIRA_DOMAIN}/browse/${1}"
+    ${open_cmd} "${JIRA_URL}/browse/${1}"
 }
 
 parse_git_branch() {
